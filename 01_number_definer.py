d34f238aedc7ab7316b4c16a14650e21d85d199f
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2023-08-18 20:31:03 +0300

    Create 01_number_definer.py

---
+++ 01_number_definer.py
@@ -0,0 +1,23 @@
+def check_number(num):
+    if num == 0:
+        print("zero")
+
+    elif num > 0:
+        if num < 1:
+            print("small positive")
+        elif num > 1_000_000:
+            print("large positive")
+        else:
+            print("positive")
+
+    else:
+        if abs(num) < 1:
+            print("small negative")
+        elif abs(num) > 1_000_000:
+            print("large negative")
+        else:
+            print("negative")
+
+
+number = float(input())
+check_number(number)
