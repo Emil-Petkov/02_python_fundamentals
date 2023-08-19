336648b15bce9103bbbda9e5ca44da10bf7b0838
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-08-20 00:08:44 +0300

    Create 08_how_much_coffee_do_you_need.py

---
+++ 1_basic_syntax_conditional_statements_and_loops/2_exe/08_how_much_coffee_do_you_need.py
@@ -0,0 +1,30 @@
+command = input()
+
+number_coffees = 0
+
+events = \
+    [
+        "coding",
+        "CODING",
+        "dog",
+        "DOG",
+        "cat",
+        "CAT",
+        "movie",
+        "MOVIE",
+    ]
+
+while not command == "END":
+
+    if command in events:
+        if command == command.lower():
+            number_coffees += 1
+        elif command == command.upper():
+            number_coffees += 2
+
+    command = input()
+
+if number_coffees > 5:
+    print("You need extra sleep")
+else:
+    print(number_coffees)
