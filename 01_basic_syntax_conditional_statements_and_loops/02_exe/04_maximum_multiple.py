fa6d76acb4dd43d2309cfd1ef6fe3c7dd5ea09df
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-02-04 14:20:48 +0200

    Create 04_maximum_multiple.py

---
+++ 01_basic_syntax_conditional_statements_and_loops/02_exe/04_maximum_multiple.py
@@ -0,0 +1,10 @@
+divisor = int(input())
+boundary = int(input())
+
+largest_multiple = divisor * (boundary // divisor)
+
+if largest_multiple > 0:
+    print(largest_multiple)
+
+else:
+    print(divisor)
