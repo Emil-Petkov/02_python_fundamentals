a981f7bd97bb1ed2bd4de03ad0f16bf90b6b46ff
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-02-26 20:59:37 +0200

    Create 03_list_statistics.py

---
+++ 03_lists_basics/01_lab/03_list_statistics.py
@@ -0,0 +1,18 @@
+n_lines = int(input())
+
+positive_numbers = []
+negative_numbers = []
+
+for _ in range(n_lines):
+    current_number = int(input())
+
+    if current_number < 0:
+        negative_numbers.append(current_number)
+        continue
+
+    positive_numbers.append(current_number)
+
+print(positive_numbers)
+print(negative_numbers)
+print(f'Count of positives: {len(positive_numbers)}')
+print(f'Sum of negatives: {sum(negative_numbers)}')
