417c8a225cd3f8508b2a785da4774af616d721da
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2024-04-17 18:50:26 +0300

    .

--- 03_lists_basics/01_lab/01_strange_zoo.py
+++
@@ -1,7 +0,0 @@
-part_of_animal = []
-
-part_of_animal.append(input())
-part_of_animal.append(input())
-part_of_animal.append(input())
-
-print(part_of_animal[::-1])
--- 03_lists_basics/01_lab/02_courses.py
+++
@@ -1,9 +0,0 @@
-n_courses = int(input())
-
-courses = []
-
-for _ in range(n_courses):
-    current_course = input()
-    courses.append(current_course)
-
-print(courses)
