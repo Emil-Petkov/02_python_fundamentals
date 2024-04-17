1f4ea71371dbf49cf6ca37dac5a94128870dc3b0
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2024-04-17 18:50:34 +0300

    .

--- 03_lists_basics/02_exe/01_invert_values.py
+++
@@ -1,8 +0,0 @@
-numbers_in_string = input().split()
-
-lst = []
-
-for number in numbers_in_string:
-    lst.append(-int(number))
-
-print(lst)
--- 03_lists_basics/02_exe/02_multiples_list.py
+++
@@ -1,9 +0,0 @@
-factor = int(input())
-count = int(input())
-
-list_of_numbers = []
-
-for i in range(1, count + 1):
-    list_of_numbers.append(factor * i)
-
-print(list_of_numbers)
