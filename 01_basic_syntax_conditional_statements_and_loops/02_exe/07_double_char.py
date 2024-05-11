87c571166bee26eff9da8af149b31cb1e17a6165
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-05-11 15:55:47 +0300

    Create 07_double_char.py

---
+++ 01_basic_syntax_conditional_statements_and_loops/02_exe/07_double_char.py
@@ -0,0 +1,15 @@
+command = input()
+
+while not command == 'End':
+    if command == 'SoftUni':
+
+        command = input()
+        continue
+
+    else:
+        for ch in command:
+            print(ch * 2, end='')
+
+        print()
+
+    command = input()
