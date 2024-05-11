1e491c61434a226483b2ef549b21cca2e1d176f1
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-05-11 14:43:46 +0300

    Create 01_jenny_secret_message.py

---
+++ 01_basic_syntax_conditional_statements_and_loops/02_exe/01_jenny_secret_message.py
@@ -0,0 +1,14 @@
+name = input()
+
+while not name == 'Johnny!':
+    print(f'Hello, {name}')
+
+    name = input()
+
+print(f'Hello, my love!')
+
+# if not name == 'Johnny':
+#     print(f'Hello, {name}!')
+#
+# else:
+#     print(f'Hello, my love!')
