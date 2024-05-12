159b47d87d043b10ad80385dc8e7d8796180df97
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-05-12 17:55:15 +0300

    Create 04_centuries_to_minutes.py

---
+++ 02_data_types_and_variables/01_lab/04_centuries_to_minutes.py
@@ -0,0 +1,10 @@
+century = int(input())
+
+years = century * 100
+days = int(years * 365.2422)
+hours = days * 24
+minutes = hours * 60
+
+result = f'{century} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes'
+
+print(result)
