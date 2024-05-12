cb0f5331f8dd2026776af2ba2699ab6602d52489
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-05-12 18:09:35 +0300

    Create 02_chars_to_string.py

---
+++ 02_data_types_and_variables/02_exe/02_chars_to_string.py
@@ -0,0 +1,11 @@
+def chars_to_string(f_char: str, s_char: str, t_char: str) -> str:
+    return f_char + s_char + t_char
+
+
+first_char = input()
+second_char = input()
+third_char = input()
+
+result = chars_to_string(first_char, second_char, third_char)
+
+print(result)
