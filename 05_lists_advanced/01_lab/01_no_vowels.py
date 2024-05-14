2c3d710d8bad6785b6f8ced84eb4659d8212cef2
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-05-14 14:30:46 +0300

    Create 01_no_vowels.py

---
+++ 05_lists_advanced/01_lab/01_no_vowels.py
@@ -0,0 +1,21 @@
+text = input()
+
+modified_string = ''
+
+vowels = \
+    [
+        'a',
+        'o',
+        'u',
+        'e',
+        'i',
+    ]
+
+for ch in text:
+    if ch.lower() in vowels:
+        continue
+
+    else:
+        modified_string += ch
+
+print(modified_string)
