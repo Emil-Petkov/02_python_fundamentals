3200df9e5f57c7a673e3f744c5de6a3231805b91
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-05-14 12:43:29 +0300

    Create 06_calculate_rectangle_area.py

---
+++ 04_functions/01_lab/06_calculate_rectangle_area.py
@@ -0,0 +1,8 @@
+def calculate_rectangle_area(side_a: int, side_b: int):
+    return side_a * side_b
+
+
+side_a = int(input())
+side_b = int(input())
+result = calculate_rectangle_area(side_a, side_b)
+print(result)
