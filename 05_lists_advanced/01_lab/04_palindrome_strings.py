d68eac5399b2850b577947e503674bb81286446e
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-05-23 13:03:34 +0300

    Create 04_palindrome_strings.py

---
+++ 05_lists_advanced/01_lab/04_palindrome_strings.py
@@ -0,0 +1,15 @@
+words = input().split()
+search_palindrome = input()
+
+all_palindromes = []
+
+counter = 0
+
+for word in words:
+    if word == word[::-1]:
+        all_palindromes.append(word)
+
+        if word == search_palindrome:
+            counter += 1
+
+print(f'{all_palindromes}\nFound palindrome {counter} times')
