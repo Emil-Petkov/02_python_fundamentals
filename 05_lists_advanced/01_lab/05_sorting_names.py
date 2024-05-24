19ba8aab4196d3839df958f81ca8a95f5d76240a
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-05-24 13:35:08 +0300

    Update 05_sorting_names.py

--- 05_lists_advanced/01_lab/05_sorting_names.py
+++ 05_lists_advanced/01_lab/05_sorting_names.py
@@ -5,3 +5,10 @@ names.sort()
 names.sort(key=len, reverse=True)
 
 print(names)
+
+# names = input().split(', ')
+# sorted_alphabetically = sorted(names)
+#
+# sorted_by_length = sorted(sorted_alphabetically, key=lambda name: -len(name))
+#
+# print(sorted_by_length)
