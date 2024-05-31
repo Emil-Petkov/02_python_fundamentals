8e8d40634fc35c6da2fbd908cd6583ba59f6e1da
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-05-31 11:39:36 +0300

    Update 03_еmail.py

--- 06_objects_and_classes/01_lab/03_еmail.py
+++ 06_objects_and_classes/01_lab/03_еmail.py
@@ -3,10 +3,31 @@ class Email:
         self.sender = sender
         self.receiver = receiver
         self.content = content
+        self.is_sent = False
+
+    def send(self):
         self.is_sent = True
 
     def get_info(self):
-        return f'{self.sender} says to {self.receiver}: {self.content}. Sent: {self.is_sent}'
+        return f"{self.sender} says to {self.receiver}: {self.content}. Sent: {self.is_sent}"
 
 
-email = Email()
+emails = []
+
+command = input().split()
+
+while not command[0] == 'Stop':
+    sender = command[0]
+    receiver = command[1]
+    content = command[2]
+
+    email = Email(sender, receiver, content)
+
+    emails.append(email)
+
+    command = input().split()
+
+send_emails = [emails[int(x)].send() for x in input().split(', ')]
+
+for email in emails:
+    print(email.get_info())
