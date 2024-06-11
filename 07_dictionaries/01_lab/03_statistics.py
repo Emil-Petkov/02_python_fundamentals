4c82b051ed957102b4261d9972e431a83861653f
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-06-11 10:11:34 +0300

    Create 03_statistics.py

---
+++ 07_dictionaries/01_lab/03_statistics.py
@@ -0,0 +1,23 @@
+data = input().split(': ')
+
+products = {
+
+}
+
+while not data[0] == 'statistics':
+    product = data[0]
+    quantity = data[1]
+
+    if product in products:
+        products[product] += int(quantity)
+    else:
+        products[product] = int(quantity)
+
+    data = input().split(': ')
+
+print('Products in stock:')
+
+for key, value in products.items():
+    print(f'- {key}: {value}')
+
+print(f'Total Products: {len(products.keys())}\nTotal Quantity: {sum(products.values())}')
