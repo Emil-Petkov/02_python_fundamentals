7bbb79292149f797ce9ded25f4b9d591553d9deb
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-06-11 10:11:55 +0300

    Create 04_students.py

---
+++ 07_dictionaries/01_lab/04_students.py
@@ -0,0 +1,23 @@
+command = input().split(':')
+
+student_information = {
+
+}
+
+while len(command) > 1:
+    name = command[0]
+    student_id = command[1]
+    course = command[2]
+
+    if course not in student_information:
+        student_information[course] = {}
+
+    student_information[course][student_id] = name
+
+    command = input().split(':')
+
+course_name = command[0].replace('_', ' ')
+
+if course_name in student_information:
+    for id, name in student_information[course_name].items():
+        print(f'{name} - {id}')
