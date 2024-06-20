f2f6cdc91ac16fc4f9d064bc8565bc71249f6952
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2024-06-21 01:51:39 +0300

    Create 01_capture_the_numbers.py

---
+++ 09_regular_expressions/02_exe/01_capture_the_numbers.py
@@ -0,0 +1,12 @@
+import re
+import sys
+
+pattern = r'\d+'
+
+numbers = []
+
+for line in sys.stdin:
+    matches = re.findall(pattern, line)
+    numbers.extend(matches)
+
+print(' '.join(numbers))
